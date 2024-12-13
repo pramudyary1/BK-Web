7be7dc691f4631dffd4238e4262b911c83cceead
pram <pramudya@gmail.com>
2024-12-13 23:45:01 +0700

    Kumpulin BK-Web Tugas 1

---
+++ pages/dashboard/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            padding: 0;
+        }
+
+        .title {
+            font-size: 1rem;
+            /* Responsive font size */
+            color: #000;
+            margin-bottom: 1.25rem;
+            font-weight: bold;
+            text-transform: uppercase;
+            text-align: center;
+            letter-spacing: 0.1rem;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+        }
+
+        .banner-section {
+            font-family: 'Sora', sans-serif;
+            background-image: url('assets/images/dashboardPasien.jpg');
+            background-position: center;
+            background-size: cover;
+            font-weight: 800;
+            color: #F5F5F5;
+            padding: 10% 0;
+            /* Responsive padding */
+            text-align: center;
+            display: flex;
+            /* Use flexbox to center content */
+            flex-direction: column;
+            /* Stack content vertically */
+            align-items: center;
+            /* Center content horizontally */
+            justify-content: center;
+            /* Center content vertically */
+            height: 100vh;
+            /* Set height to 100% of viewport height */
+        }
+
+
+   
+    </style>
+</head>
+
+<body>
+    <!-- Section 1: Banner -->
+    <section class="banner-section">
+        <div class="container text-right my-5 fade-in">
+            <h1 style="text-align: center; font-size: 30px;"><b>Kesehatan Anda Prioritas Kami</b></h1>
+            <p style="text-align: center; font-size: 20px;">Kami akan memberikan pelayanan terbaik kami hingga anda
+                sembuh</p>
+        </div>
+    </section>
+
+
+
+
+
+</body>
+
+</html>
